0375d92fa00981f990e705ba74a73d783d91fa73
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-29 22:32:15 +0100

    Lesson 9. Passing data into templates

---
+++ 02_parse_data_templates/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", "ice cream")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
