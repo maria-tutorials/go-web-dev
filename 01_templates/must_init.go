0aec54ca65973c9c3823482b6ab18a424a7c0158
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-09-29 22:25:48 +0100

    Lesson 8. Parsing and executing templates

---
+++ 01_templates/must_init.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	err := tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "chocolate.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "strawberry.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.ExecuteTemplate(os.Stdout, "vanilla.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
