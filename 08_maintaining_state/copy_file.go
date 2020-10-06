46153e2739c5b8a97bca5bc9e3f0384bda78be1a
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-06 20:22:07 +0100

    56. Uploading a file, reading the file, creating a file on the server

---
+++ 08_maintaining_state/copy_file.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	var s string
+
+	if req.Method == http.MethodPost {
+		s = handleFile(w, req)
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="q">
+	<input type="submit">
+	</form>
+	<br>`)
+}
+
+func handleFile(w http.ResponseWriter, req *http.Request) string {
+	// open
+	f, h, err := req.FormFile("q")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return ""
+	}
+	defer f.Close()
+
+	// for your information
+	fmt.Println("\nfile:", f, "\nheader:", h, "\nerr", err)
+
+	// read
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return ""
+	}
+
+	storeFile(w, h.Filename, bs)
+
+	return string(bs)
+}
+
+func storeFile(w http.ResponseWriter, filename string, bs []byte) {
+	// store on server
+	dst, err := os.Create(filepath.Join("./user/", filename))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	_, err = dst.Write(bs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
