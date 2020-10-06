e7709a00448c7655652cc91e8dc245daf7d7f3a3
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-06 20:47:22 +0100

    60. Redirects - in practice
    303

---
+++ 08_redirects/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/seeOther", seeOtherHandler)
+	http.HandleFunc("/barred", barred)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Print("Your request method at /: ", req.Method, "\n\n")
+}
+
+func seeOtherHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at /seeOther:", req.Method)
+	http.Redirect(w, req, "/", http.StatusSeeOther)
+}
+
+func barred(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Your request method at barred:", req.Method)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
