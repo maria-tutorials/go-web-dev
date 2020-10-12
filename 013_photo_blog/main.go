4bfc48037b0a9ea1bb002839c7162b7a36317aed
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-12 19:53:19 +0100

    98. Storing Multiple Values

--- 013_photo_blog/main.go
+++ 013_photo_blog/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"strings"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -29,7 +30,9 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	c := getCookie(w, req)
-	tpl.ExecuteTemplate(w, "index.gohtml", c)
+	c = appendMultipleValues(w, c)
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 }
 
 func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
@@ -45,5 +48,21 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 		}
 		http.SetCookie(w, c)
 	}
+	return c
+}
+
+func appendMultipleValues(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	p1 := "pic1.jpg"
+	p2 := "pic2.jpg"
+	p3 := "pic3.jpg"
+	ps := []string{c.Value, p1, p2, p3}
+
+	if !strings.Contains(c.Value, p1) { // cheating because I know I always set them all together gg
+		s := strings.Join(ps, "|")
+		c.Value = s
+
+		http.SetCookie(w, c)
+	}
+
 	return c
 }
