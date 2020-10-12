938e5ce92413790b8611ca82873795987a244869
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-12 20:10:42 +0100

    99. Uploading Pictures
     copying from multipart file to File

--- 013_photo_blog/main.go
+++ 013_photo_blog/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 
 	uuid "github.com/satori/go.uuid"
@@ -30,7 +35,37 @@ func main() {
 
 func index(w http.ResponseWriter, req *http.Request) {
 	c := getCookie(w, req)
-	c = appendMultipleValues(w, c)
+
+	// process form submission
+	if req.Method == http.MethodPost {
+		mf, fh, err := req.FormFile("f")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		// create sha for file name from FileHeader
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+		// create new file
+		wd, err := os.Getwd() //dir
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pictures", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		// copy
+		mf.Seek(0, 0) //get head back to begining
+		io.Copy(nf, mf)
+		// add filename to this user's cookie
+		c = appendValue(w, c, fname)
+	}
+
 	xs := strings.Split(c.Value, "|")
 	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 }
@@ -51,6 +86,7 @@ func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
 	return c
 }
 
+//deprecated
 func appendMultipleValues(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
 	p1 := "pic1.jpg"
 	p2 := "pic2.jpg"
@@ -64,5 +100,15 @@ func appendMultipleValues(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
 		http.SetCookie(w, c)
 	}
 
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
 	return c
 }
