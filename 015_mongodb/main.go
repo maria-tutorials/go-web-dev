3b2b2e0ed48b1402fa8ede1848f414c425dad25e
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-13 19:22:27 +0100

    113. Organizing Code Into Packages
    reply with fake user on route

--- 015_mongodb/main.go
+++ 015_mongodb/main.go
@@ -1,18 +1,61 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+
+	"./models"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	r := httprouter.New()
+
 	r.GET("/", index)
-	http.ListenAndServe("localhost:8080", r)
+	r.GET("/user/:id", getUser)
+
+	fmt.Println("Server started at port 8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
 
-func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	s := `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+			<meta charset="UTF-8">
+			<title>Index</title>
+			</head>
+			<body>
+			<a href="/user/123456789">CHECK OUT: http://localhost:8080/user/123456789</a>
+			</body>
+			</html>
+		`
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(s))
+}
+
+func getUser(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	u := models.User{
+		ID:     p.ByName("id"),
+		Name:   "FAKE USER",
+		Gender: "fake",
+		Age:    100,
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "ups something went wrong")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
 }
