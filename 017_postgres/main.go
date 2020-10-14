72071d21f46929021a04ecb79a562728bc4aa66a
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-14 22:31:03 +0100

    149. Web App

--- 017_postgres/main.go
+++ 017_postgres/main.go
@@ -2,53 +2,84 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
-// Book order and naming matches the table
-type Book struct {
-	isbn   string
-	title  string
-	author string
-	price  float32
-}
+var db *sql.DB
 
-func main() {
-	db, err := sql.Open("postgres", "postgres://maria:password@localhost/bookstore?sslmode=disable")
+func init() {
+	var err error
+
+	db, err = sql.Open("postgres", "postgres://maria:password@localhost/bookstore?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
 	if err = db.Ping(); err != nil {
 		panic(err)
 	}
 	fmt.Println("You connected to your database.")
+}
+
+// Book order and naming matches the table
+type Book struct {
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Price  float32 `json:"price"`
+}
+
+func main() {
+	http.HandleFunc("/books", booksHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func booksHandler(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
 
 	rows, err := db.Query("SELECT * FROM books;")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
 
 	bks := make([]Book, 0)
 	for rows.Next() {
 		bk := Book{}
-		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+		err := rows.Scan(&bk.Isbn, &bk.Title, &bk.Author, &bk.Price)
 		if err != nil {
-			panic(err)
+			fmt.Println(err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
 		}
 		bks = append(bks, bk)
 	}
 	if err = rows.Err(); err != nil {
-		panic(err)
+		fmt.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
 
-	for _, bk := range bks {
-		fmt.Printf("%s, %s, %s, $%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+	jbks, err := json.Marshal(bks)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
 	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", jbks)
 }
 
 // psql
