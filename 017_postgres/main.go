788abdca1e286a7f862361818ff75e41bb36a746
Maria Inês Serra <14142563+mariaines00@users.noreply.github.com>
2020-10-14 22:13:19 +0100

    148. Select Query

--- 017_postgres/main.go
+++ 017_postgres/main.go
@@ -7,6 +7,14 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Book order and naming matches the table
+type Book struct {
+	isbn   string
+	title  string
+	author string
+	price  float32
+}
+
 func main() {
 	db, err := sql.Open("postgres", "postgres://maria:password@localhost/bookstore?sslmode=disable")
 	if err != nil {
@@ -14,11 +22,33 @@ func main() {
 	}
 	defer db.Close()
 
-	err = db.Ping()
-	if err != nil {
+	if err = db.Ping(); err != nil {
 		panic(err)
 	}
 	fmt.Println("You connected to your database.")
+
+	rows, err := db.Query("SELECT * FROM books;")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	bks := make([]Book, 0)
+	for rows.Next() {
+		bk := Book{}
+		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+		if err != nil {
+			panic(err)
+		}
+		bks = append(bks, bk)
+	}
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+
+	for _, bk := range bks {
+		fmt.Printf("%s, %s, %s, $%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
+	}
 }
 
 // psql
